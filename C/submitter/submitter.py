fddfb74dceac87e31be1f8ead63995fb491d1438
Michal Kawalec <michal@bazzle.me>
2013-10-18 11:30:35 +0100

    ssh connections run in parallel

--- C/submitter/submitter.py
+++ C/submitter/submitter.py
@@ -6,18 +6,24 @@ Logs in to cplab computers and launches jobs in parallel
 
 from multiprocessing import Process
 from subprocess import call
-from progressbar import Bar, ETA, Percentage, ProgressBar
-from time import sleep
+#from progressbar import Bar, ETA, Percentage, ProgressBar
+#from time import sleep
 
 def setup_remote(host):
-    call(["ssh s0905879@%(host)s \' cd /dev/shm; "
-         "git clone https://github.com/mkawalec/masters turb; "
-         "cd turb; mkdir build; cd build; cmake ..; make -j3; "
-         "./integrator 7 0.0005 1000 10" % (host=host)], shell=True)
-    call(["scp s0905879@%(host)s:/dev/shm/turb/build/output %(host)s.out" % \
-            (host=host)], shell=True)
+    call(["ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "
+          "s0905879@%(host)s \' cd /dev/shm; "
+          "git clone https://github.com/mkawalec/masters turb; "
+          "cd turb; mkdir build; cd build; cmake ..; make -j3; "
+          "./integrator 7 0.0005 1000 10" % dict(host=host)], shell=True)
+    call(["scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "
+          "s0905879@%(host)s:/dev/shm/turb/build/output %(host)s.out" % \
+            dict(host=host)], shell=True)
 
 
 if __name__ == '__main__':
-    for i in xrange(10):
-        setup_remote("cplab%03d" % (i,))
+    processes = []
+    for i in range(10):
+        processes.append(Process(target=setup_remote, args=["cplab%03d" % (i,)]))
+
+    for process in processes:
+        process.start()
