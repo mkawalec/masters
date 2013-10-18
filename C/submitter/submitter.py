82e6dffe7cfddcc3b79ac972f5c88f1ed08ae180
Michal Kawalec <michal@bazzle.me>
2013-10-18 13:04:49 +0100

    accepting command-line options

--- C/submitter/submitter.py
+++ C/submitter/submitter.py
@@ -8,25 +8,29 @@ from multiprocessing import Process
 from subprocess import call
 from progressbar import Bar, ETA, Percentage, ProgressBar
 from time import sleep
+from sys import argv
 
 devnull = open("/dev/null", "w")
 
-def setup_remote(host):
+def setup_remote(host, runs):
     call(["ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "
           "s0905879@%(host)s \' cd /dev/shm; rm -rf turb ;"
           "git clone -b ssh_bomb https://github.com/mkawalec/masters turb; "
           "cd turb/C; mkdir build; cd build; cmake ..; make -j3; "
-          "./integrator 7 0.0005 1000 10\'" % dict(host=host)], shell=True,
-          stdout=devnull, stderr=devnull)
+          "./integrator 7 0.0005 1000 %(runs)d\'" % dict(host=host, runs=runs)], 
+          shell=True, stdout=devnull, stderr=devnull)
+
     call(["scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "
           "s0905879@%(host)s:/dev/shm/turb/C/build/output %(host)s.out" % \
             dict(host=host)], shell=True, stderr=devnull, stdout=devnull)
 
-
 if __name__ == '__main__':
+    hosts = int(argv[1])
+    runs = int(argv[2])
+
     processes = []
-    for i in range(20):
-        processes.append(Process(target=setup_remote, args=["cplab%03d" % (i,)]))
+    for i in range(hosts):
+        processes.append(Process(target=setup_remote, args=["cplab%03d" % (i,), runs]))
 
     for process in processes:
         process.start()
