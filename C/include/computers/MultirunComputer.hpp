0d7710a9f1a5ca59af1750f59dd2b1aa3d4b3925
Michal Kawalec <michal@bazzle.me>
2013-11-14 14:33:29 +0000

    multirun computer is commented

--- C/include/computers/MultirunComputer.hpp
+++ C/include/computers/MultirunComputer.hpp
@@ -8,16 +8,32 @@
 
 namespace turb {
 
+    /*! \brief Base class for computers that need
+     *      to run more than one run to get useful data.
+     */
     template <typename T>
     class MultirunComputer : public Computer {
     private:
+        /*! \brief Fit a decay profile of a 
+         *      vector of decay times to an e^-ax function
+         */
         void fit_it(std::vector<double> *decay_times);
+
+        /*! \brief The proportion of tail of the 
+         *      decay times that will be used for the fit.
+         */
         double fit_part = 0.3;
 
     protected:
+        /*! \brief Manages multiple runs, reduces their
+         *      output data etc.
+         */
         void compute();
 
     public:
+        /*! \brief Implemented by derived classes to execute
+         *      a single run.
+         */
         virtual double compute_single(std::ofstream *output) = 0;
 
     };
