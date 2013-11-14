c354813ca05c7fc2a5cc0fa03c90ed5624b1a5f0
Michal Kawalec <michal@bazzle.me>
2013-11-14 13:50:24 +0000

    commented Serializer

--- C/include/Serializer.hpp
+++ C/include/Serializer.hpp
@@ -8,11 +8,31 @@
 
 namespace turb {
 
+    /*! \brief Serializes the program data to some
+     *      output stream
+     *
+     *  Every Serializer needs to be stateless,
+     *  keeping any kind of state information inside
+     *  a Serializer class will most probably result
+     *  in nasty and hard to trace bugs
+     */
     class Serializer : public Base<Serializer> {
     public:
+        /*! \brief Function called to persist current
+         *      integrator state.
+         *
+         *  \param integrator integrator instance from
+         *      which data will be taken
+         *  \param output output stream
+         *  \param time current time, its role depends
+         *      on output method used
+         */
         virtual void serialize(Integrator *integrator, 
                 std::ofstream *output, void *time) = 0;
 
+        /*! \brief Base::clone overload to return
+         *      a correct pointer type
+         */
         virtual Serializer* clone() const = 0;
     };
 }
