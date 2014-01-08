5ebc84e08cb361bcec9c14fdb37bf181e484f800
Michal Kawalec <michal@bazzle.me>
2014-01-08 21:15:52 +0000

    wonderful code duplication to make it compatible with old GCC

--- include/exceptions.hpp
+++ include/exceptions.hpp
@@ -38,32 +38,57 @@ namespace turb {
      *      could not be found at the moment.
      */
     struct InstanceNotFound : public Exception {
-        using Exception::Exception;
+        //using Exception::Exception;
+    public:
+        InstanceNotFound() { message = ""; }
+        InstanceNotFound(std::string msg) { message = msg; }
+        InstanceNotFound(std::stringstream *output) { message = output->str();}
+        InstanceNotFound(double num) { std::stringstream tmp; tmp << num; message = tmp.str();}
     };
 
-    /*! \brief Indicates a fatal error requiring 
+    /*! \brief Indicates a fatal error requiring
      *      immediate program termination.
      */
     struct ProgramDeathRequest : public Exception {
-        using Exception::Exception;
+        //using Exception::Exception;
+    public:
+        ProgramDeathRequest() { message = ""; }
+        ProgramDeathRequest(std::string msg) { message = msg; }
+        ProgramDeathRequest(std::stringstream *output) { message = output->str();}
+        ProgramDeathRequest(double num) { std::stringstream tmp; tmp << num; message = tmp.str();}
     };
 
     /*! \brief Thrown when currently used output file
      *      needs to be deleted properly.
      */
     struct RemoveOutput : public Exception {
-        using Exception::Exception;
+        //using Exception::Exception;
+    public:
+        RemoveOutput() { message = ""; }
+        RemoveOutput(std::string msg) { message = msg; }
+        RemoveOutput(std::stringstream *output) { message = output->str();}
+        RemoveOutput(double num) { std::stringstream tmp; tmp << num; message = tmp.str();}
     };
 
     /*! \brief Thrown if there is no result available
      *      to be thrown.
      */
     struct NoResult : public Exception {
-        using Exception::Exception;
+        //using Exception::Exception;
+    public:
+        NoResult() { message = ""; }
+        NoResult(std::string msg) { message = msg; }
+        NoResult(std::stringstream *output) { message = output->str();}
+        NoResult(double num) { std::stringstream tmp; tmp << num; message = tmp.str();}
     };
 
     struct OutOfBounds : public Exception {
-        using Exception::Exception;
+        //using Exception::Exception;
+    public:
+        OutOfBounds() { message = ""; }
+        OutOfBounds(std::string msg) { message = msg; }
+        OutOfBounds(std::stringstream *output) { message = output->str();}
+        OutOfBounds(double num) { std::stringstream tmp; tmp << num; message = tmp.str();}
     };
 }
 
