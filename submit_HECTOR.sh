e4aa3fbb27296c77619fabe7cc6f8ecb7b03423a
Michal Kawalec <michal@bazzle.me>
2014-01-08 22:49:10 +0000

    submitting works with correct details

--- submit_HECTOR.sh
+++ submit_HECTOR.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#$ -V
-#$ -cwd
+#PBS -N Integrator
+#PBS -l mppwidth=1
+#PBS -l mppnppn=1
+#PBS -l walltime=00:05:00
+#PBS -A d54
 
-mpirun -np $NSLOTS ./integrator -c decay-path -r 1 -f 0 --fast-threshold 1000 --static-interval 1000
+aprun -n 1 -N 1 /home/d54/d54/s0905879/masters/build/integrator -c decay-path -r 1 -f 0 --fast-threshold 1000 --static-interval 1000
