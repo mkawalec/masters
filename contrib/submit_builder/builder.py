9e656b2e2e4c2d2b7d8dad0c05fa978da5dd3b5f
Michal Kawalec <michal@bazzle.me>
2014-03-05 13:02:27 +0000

    domain size is set correctly

--- contrib/submit_builder/builder.py
+++ contrib/submit_builder/builder.py
@@ -14,9 +14,9 @@ def create_submitters():
             # R range from 0.9 to 1.09
             for R in [x * 0.01 for x in range(90, 110)]:
                 f.write("aprun -n 256 -N 32 /work/d54/d54/s0905879/integrator "
-                        "-c decay-mult -r 40 -f 1 --end-time 7000 "
+                        "-c decay-mult -r 40 -f 1 --end-time 7000 -d %s "
                         "--find-zeros 0 --use-output 0 -R %s --prefix '%s'\n"
-                        % (str(R), str(domain) + '-' + str(R) + '-'))
+                        % (str(domain * pi), str(R), str(domain) + '-' + str(R) + '-'))
 
 if __name__ == '__main__':
     create_submitters()
