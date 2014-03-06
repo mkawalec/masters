5959aee99e06571dddcf3e52ecd6d22497d37b1a
Michal Kawalec <michal@bazzle.me>
2014-03-06 13:28:25 +0000

    a new builder version

--- contrib/submit_builder/builder.py
+++ contrib/submit_builder/builder.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python2
 
 from math import pi
+from sys import argv
 
 def create_submitters():
-    with open('scaffold.tpl', 'r') as f:
+    with open(argv[1], 'r') as f:
         template = f.read()
 
     # Domain size from 24 to 2 pi
@@ -13,7 +14,7 @@ def create_submitters():
 
             # R range from 0.9 to 1.09
             for R in [x * 0.01 for x in range(90, 110)]:
-                f.write("aprun -n 256 -N 32 /work/d54/d54/s0905879/integrator "
+                f.write("aprun -n 240 -N 24 /work/d54/d54/s0905879/integrator "
                         "-c decay-mult -r 40 -f 1 --end-time 7000 -d %s "
                         "--find-zeros 0 --use-output 0 -R %s --prefix '%s'\n"
                         % (str(domain * pi), str(R), str(domain) + '-' + str(R) + '-'))
