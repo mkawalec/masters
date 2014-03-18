b6b1d786da7da9c18647e2607bbaba82bfeb06bf
Michal Kawalec <michal@bazzle.me>
2014-03-18 20:56:09 +0000

    added builder function

--- contrib/submit_builder/builder.py
+++ contrib/submit_builder/builder.py
@@ -2,11 +2,33 @@
 
 from math import pi
 from sys import argv
+import numpy as np
+
 
 def create_submitters():
     with open(argv[1], 'r') as f:
         template = f.read()
 
+    if argv[2] == 'mult':
+        create_input_mult(template)
+    else:
+        create_diff_domains(template)
+
+
+def create_input_mult(template):
+    output = open("submit-input-mult", "w")
+    output.write(template)
+
+    for mult in np.logspace(0, 2, 20):
+        output.write("aprun -n 2400 -N 24 /work/d59/d59/s0905879/integrator "
+                     "-c decay-mult -r 40 -f 1 --end-time 7000 --dt 0.00005 "
+                     "--start-mult %f --find-zeros 0 --use-output 0 --prefix '%s'\n" %
+                     (mult, 'input-mult-' + str(mult) + '-'))
+
+    output.close()
+
+
+def create_diff_domains(template):
     # Domain size from 24 to 2 pi
     for domain in range(24, 0, -2):
         with open("submit-" + str(domain) + 'pi.sh', 'w') as f:
@@ -18,6 +40,7 @@ def create_submitters():
                         "-c decay-mult -r 40 -f 1 --end-time 7000 -d %s "
                         "--find-zeros 0 --use-output 0 -R %s --prefix '%s'\n"
                         % (str(domain * pi), str(R), str(domain) + '-' + str(R) + '-'))
+
 
 if __name__ == '__main__':
     create_submitters()
